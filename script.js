e7fe8b001b440b9c2c8379476b82b1a97b254f9a
Michael Grosse <grosse@cosmocode.de>
2017-02-06 21:44:37 +0100

    Use jQuery

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-console.log('jsstart');
-const websocket = new WebSocket('ws://localhost:9000/~michael/dokuwiki/lib/plugins/websockets/server.php');
-websocket.onopen = function (evt) {
-    console.log('open');
-    console.dir(evt);
-    websocket.send('{"msg":"testmessage"}');
-};
-websocket.onclose = function (evt) {
-    console.log('close');
-    //console.dir(evt);
-};
-websocket.onmessage = function (evt) {
-    console.log('message');
-    console.dir(evt);
-    console.log(evt.data);
-    console.log(evt.data.length);
-};
-websocket.onerror = function (evt) {
-    console.log('error');
-    console.dir(evt);
-};
-console.log('jsdone');
+jQuery(function(){
+    'use strict';
+    console.log('jsstart');
+    const websocket = new WebSocket('ws://127.0.0.1:9000/~michael/dokuwiki/');//~michael/dokuwiki/lib/plugins/websockets/server.php');
+    websocket.onopen = function (evt) {
+        console.log('open');
+        console.dir(evt);
+        websocket.send('{"msg":"testmessage"}');
+    };
+    websocket.onclose = function (evt) {
+        console.log('close');
+        //console.dir(evt);
+    };
+    websocket.onmessage = function (evt) {
+        console.log('message');
+        console.dir(evt);
+        console.log(evt.data);
+        console.log(evt.data.length);
+    };
+    websocket.onerror = function (evt) {
+        console.log('error');
+        console.dir(evt);
+    };
+    console.log('jsdone');
+});
